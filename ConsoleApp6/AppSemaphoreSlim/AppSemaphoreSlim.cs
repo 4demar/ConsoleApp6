cf293c58cd9d2c5166a276595d6e5745b6cd8909
Ademar de Souza Novaes Junior <ademar.junior@havan.com.br>
2025-01-09 10:42:25 -0300

    Adicionar arquivos de projeto.

---
+++ ConsoleApp6/AppSemaphoreSlim/AppSemaphoreSlim.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ConsoleApp6.AppSemaphoreSlim
+{
+    public class AppSemaphoreSlim
+    {
+        private readonly SemaphoreSlim semaphore = new SemaphoreSlim(2);
+        public async Task<bool> IniciarApp()
+        {
+            // Libera 2 thread por execução
+
+            // Simula tarefas tentando acessar o recurso
+            Task[] tasks = new Task[5];
+            for (int i = 0; i < tasks.Length; i++)
+            {
+                int taskNumber = i + 1; // Apenas para identificação
+                tasks[i] = Task.Run(async () =>
+                {
+                    Console.WriteLine($"Task {taskNumber} esperando para acessar o recurso.");
+                    await semaphore.WaitAsync(); // Aguarda disponibilidade
+                    try
+                    {
+                        Console.WriteLine($"Task {taskNumber} executando o recurso.");
+                        await Task.Delay(3000); // Simula trabalho
+                    }
+                    finally
+                    {
+                        Console.WriteLine($"Task {taskNumber} liberou o acesso ao recurso.");
+                        semaphore.Release(); // Libera o recurso
+                    }
+                });
+
+                // Ou aplique o semaphore no metodo 
+                // await ExecutarRecurso(taskNumber);
+            }
+
+            // Aguarda todas as tarefas concluírem
+            // Caso houver lista de tarefas...
+            await Task.WhenAll(tasks);
+
+            Console.WriteLine("Todas as tarefas concluídas.");
+
+            return true;
+        }
+
+        public async Task ExecutarRecurso(int taskNumber)
+        {
+            Console.WriteLine($"Task {taskNumber} esperando para acessar o recurso.");
+
+            // só executa se houver disponibilidade
+            await semaphore.WaitAsync(); // Aguarda disponibilidade
+            try
+            {
+                Console.WriteLine($"Task {taskNumber} acessou o recurso.");
+                await Task.Delay(5000); // Simula Executando trabalho
+            }
+            finally
+            {
+                Console.WriteLine($"Task {taskNumber} liberou o recurso.");
+                semaphore.Release(); // Libera o recurso
+            }
+        }
+    }
+}
+
+
+//Exemplo de Execução
+//    Task 1 esperando para acessar o recurso.
+//    Task 1 executando o recurso.
+//    Task 3 esperando para acessar o recurso.
+//    Task 3 executando o recurso.
+//    Task 2 esperando para acessar o recurso.
+//    Task 4 esperando para acessar o recurso.
+//    Task 5 esperando para acessar o recurso.
+//    Task 3 liberou o acesso ao recurso.
+//    Task 1 liberou o acesso ao recurso.
+//    Task 2 executando o recurso.
+//    Task 4 executando o recurso.
+//    Task 4 liberou o acesso ao recurso.
+//    Task 2 liberou o acesso ao recurso.
+//    Task 5 executando o recurso.
+//    Task 5 liberou o acesso ao recurso.
---
+++ ConsoleApp6/Program.cs
@@ -0,0 +1,35 @@
+﻿
+
+using ConsoleApp6.AppSemaphoreSlim;
+
+var semaphoroSlim = new AppSemaphoreSlim();
+
+
+var retorno = false;
+
+do
+{
+    Console.Clear();
+    Console.WriteLine("Fala dev..");
+    Console.WriteLine("Qual aplicação deseja iniciar?");
+    Console.WriteLine(@"
+1 - SemaphoroSlim
+    ");
+
+
+    var valorDigitado = "1"; //Console.ReadLine();
+    int.TryParse(valorDigitado, out int numeroApp);
+
+    Console.Clear();
+
+    var retornoApp = numeroApp switch
+    {
+        1 => await semaphoroSlim.IniciarApp(),
+        2 => false,
+
+        _ => false
+    };
+
+    retorno = retornoApp;
+}
+while (!retorno);
